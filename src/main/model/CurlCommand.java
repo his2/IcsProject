e5862dedefdef391281a58a09900f2e3ccfced10
Peter .R Gomez <pete.gomez@sap.com>
2024-06-06 13:33:47 -0700

    changesToCurlCommand

--- src/main/model/CurlCommand.java
+++ src/main/model/CurlCommand.java
@@ -5,18 +5,30 @@ package main.model;
        -H 'documentFormatAs: json' \
       -H 'Authorization: Bearer $JWT'\" \
        > ~/Downloads/%s.json;
+       Additional note:
+When you run a Python script with the -H flag, it requests that the security policy sets the HOME environment
+variable to the home directory of the target user (usually root by default) as specified in the password database.
+This behavior ensures that the script runs with the correct user context, including the appropriate home directory.
 */
 public class CurlCommand {
     private String method;
+    //method describes what the curl command will do "get"
     private String url;
-    private String ignoreDocStatus;
+    //The url is everything from https to the ? after "transactions"
+    private boolean ignoreDocStatus;
+    //We usually don't care about the status of the document, we just want to obtain it, so "true" is the default
+    //The next three are Python using -H as defined above to run commands in the FI database.
     private String  accept;
+    //If I understand correctly, this is set to obtain the existing information from the DB, which is the json format
     private String documentFormat;
+    // Python selects "json" from the list of available document formats for the format output
     private String authorization;
+    //Python sends the JWT provided from the first process
     private String destination;
+    //Python sends the file to the user's download directory
     private String output;
 
-    public CurlCommand(String method, String url, String ignoreDocStatus, String accept, String documentFormat, String authorization, String destination, String output) {
+    public CurlCommand(String method, String url, boolean ignoreDocStatus, String accept, String documentFormat, String authorization, String destination, String output) {
         this.method = method;
         this.url = url;
         this.ignoreDocStatus = ignoreDocStatus;
